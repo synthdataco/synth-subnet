594c392fb1d18350cc1d873057853567c61ee875
Thykof <thykof@protonmail.ch>
2026-06-04 23:16:20 +0200

    update tests

--- tests/test_rewards.py
+++ tests/test_rewards.py
@@ -109,6 +109,6 @@ def test_get_rewards(db_engine):
     assert max(prompt_scores) == percentile90 - lowest_crps
 
     assert detailed_info[0]["miner_uid"] == miner_uids[0]
-    assert len(detailed_info[0]["crps_data"]) == 350
+    assert len(detailed_info[0]["crps_data"]) == 5
     assert real_prices is not None
     assert len(real_prices) == 289
